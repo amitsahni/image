6c16d95c429e4eb744252ee6b93c2c13396fdd1e
amitsahni <sahni1987@gmail.com>
2017-08-20 00:07:03 +0530

    Added new Glide 4 Update ReadMe

--- image/src/main/java/com/imageutil/ImageUtil.java
+++ image/src/main/java/com/imageutil/ImageUtil.java
@@ -48,30 +48,6 @@ public class ImageUtil {
      * @param url     the url
      * @return the builder
      */
-    @Deprecated
-    public static ImageParam.Builder with(@NonNull Context context, @NonNull String url) {
-        return new ImageParam.Builder(context, url, null);
-    }
-
-    /**
-     * With builder.
-     *
-     * @param context the context
-     * @param url     the url
-     * @return the builder
-     */
-    @Deprecated
-    public static ImageParam.Builder with(@NonNull Activity context, @NonNull String url) {
-        return new ImageParam.Builder(context, url, null);
-    }
-
-    /**
-     * With builder.
-     *
-     * @param context the context
-     * @param url     the url
-     * @return the builder
-     */
     public static ImageParam.Builder with(@NonNull Context context, @NonNull String url, @NonNull ImageView imageView) {
         return new ImageParam.Builder(context, url, imageView);
     }
