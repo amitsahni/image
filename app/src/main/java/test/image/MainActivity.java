3667f84ea844f0d214f845e213eb4b14e1eb3b1e
Amit <amit@clickapps.co>
2017-08-31 14:31:41 +0530

    Changes in  app demo

--- app/src/main/java/test/image/MainActivity.java
+++ app/src/main/java/test/image/MainActivity.java
@@ -1,12 +1,10 @@
 package test.image;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
-import com.imageutil.ImageParam;
 import com.imageutil.ImageUtil;
 
 import butterknife.BindView;
@@ -19,7 +17,21 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.imageView)
     ImageView imageView;
-    private final String url = "http://res.cloudinary.com/clickapp/image/upload/h_1440,w_2960/v1503404664/monh/staging/categories/Protein_Arabic.jpg";
+    @BindView(R.id.imageView1)
+    ImageView imageView1;
+    @BindView(R.id.imageView2)
+    ImageView imageView2;
+    @BindView(R.id.imageView3)
+    ImageView imageView3;
+    @BindView(R.id.imageView4)
+    ImageView imageView4;
+    @BindView(R.id.imageView5)
+    ImageView imageView5;
+    @BindView(R.id.imageView6)
+    ImageView imageView6;
+    @BindView(R.id.imageView7)
+    ImageView imageView7;
+    private final String url = "http://res.cloudinary.com/clickapp/image/upload/h_300,w_800/v1503404664/monh/staging/categories/Protein_Arabic.jpg";
 
 
     @Override
@@ -29,6 +41,29 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         ImageUtil.with(this, url, imageView)
                 .scaleType(ImageView.ScaleType.CENTER_INSIDE)
+                .build();
+        ImageUtil.with(this, url, imageView1)
+                .scaleType(ImageView.ScaleType.CENTER_CROP)
+                .build();
+        ImageUtil.with(this, url, imageView2)
+                .scaleType(ImageView.ScaleType.FIT_XY)
+                .build();
+
+        ImageUtil.with(this, url, imageView3)
+                .scaleType(ImageView.ScaleType.CENTER)
+                .build();
+        ImageUtil.with(this, url, imageView4)
+                .scaleType(ImageView.ScaleType.FIT_END)
+                .build();
+        ImageUtil.with(this, url, imageView5)
+                .scaleType(ImageView.ScaleType.MATRIX)
+                .build();
+        ImageUtil.with(this, url, imageView6)
+                .scaleType(ImageView.ScaleType.FIT_CENTER)
+                .build();
+
+        ImageUtil.with(this, url, imageView7)
+                .scaleType(ImageView.ScaleType.FIT_START)
                 .build();
     }
 
