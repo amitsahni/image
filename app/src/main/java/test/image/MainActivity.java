1f0425535bcfcffb1b0fd8e3c15aebe90760d5fe
Amit <amit@clickapps.co>
2017-12-08 17:39:17 +0530

    Merge to master

--- app/src/main/java/test/image/MainActivity.java
+++ app/src/main/java/test/image/MainActivity.java
@@ -1,13 +1,10 @@
 package test.image;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
-import com.imageutil.ImageConfiguration;
-import com.imageutil.ImageParam;
 import com.imageutil.ImageUtil;
 
 import butterknife.BindView;
@@ -34,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView6;
     @BindView(R.id.imageView7)
     ImageView imageView7;
-    private final String url = "http://res.cloudinary.com/clickapp/image/upload/h_1440,w_2960/v1503404664/monh/staging/categories/Protein_Arabic.jpg";
+    private final String url = "http://res.cloudinary.com/clickapps/image/upload/v1504245457/test/1024x1024-Wallpapers-010.jpg";
 
 
     @Override
@@ -42,48 +39,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//        ImageUtil.with(this, url, imageView)
-//                .scaleType(ImageView.ScaleType.FIT_CENTER)
-//                .build();
-        new ImageConfiguration.Builder()
-                .timeOut(50 * 1000L, 60 * 1000L)
-                .isDebug(false)
-                .config();
-        ImageUtil.with(this, url)
+        ImageUtil.with(this, url, imageView)
                 .scaleType(ImageView.ScaleType.FIT_CENTER)
-                .needBitmap(true, 1)
-                .callback(new ImageParam.onCallback() {
-                    @Override
-                    public void onBitmapReceived(Bitmap bitmap, int taskId) {
-                        if (bitmap != null) {
-                            imageView.setImageBitmap(bitmap);
-                        }
-                    }
-                })
                 .build();
-//        ImageUtil.with(this, url, imageView1)
-//                .scaleType(ImageView.ScaleType.FIT_CENTER)
-//                .build();
-//        ImageUtil.with(this, url, imageView2)
-//                .scaleType(ImageView.ScaleType.FIT_XY)
-//                .build();
-//
-//        ImageUtil.with(this, url, imageView3)
-//                .scaleType(ImageView.ScaleType.CENTER)
-//                .build();
-//        ImageUtil.with(this, url, imageView4)
-//                .scaleType(ImageView.ScaleType.FIT_END)
-//                .build();
-//        ImageUtil.with(this, url, imageView5)
-//                .scaleType(ImageView.ScaleType.MATRIX)
-//                .build();
-//        ImageUtil.with(this, url, imageView6)
-//                .scaleType(ImageView.ScaleType.FIT_CENTER)
-//                .build();
-//
-//        ImageUtil.with(this, url, imageView7)
-//                .scaleType(ImageView.ScaleType.FIT_START)
-//                .build();
+        ImageUtil.with(this, url, imageView1)
+                .scaleType(ImageView.ScaleType.FIT_CENTER)
+                .build();
+        ImageUtil.with(this, url, imageView2)
+                .scaleType(ImageView.ScaleType.FIT_XY)
+                .build();
+
+        ImageUtil.with(this, url, imageView3)
+                .scaleType(ImageView.ScaleType.CENTER)
+                .build();
+        ImageUtil.with(this, url, imageView4)
+                .scaleType(ImageView.ScaleType.FIT_END)
+                .build();
+        ImageUtil.with(this, url, imageView5)
+                .scaleType(ImageView.ScaleType.MATRIX)
+                .build();
+        ImageUtil.with(this, url, imageView6)
+                .scaleType(ImageView.ScaleType.FIT_CENTER)
+                .build();
+
+        ImageUtil.with(this, url, imageView7)
+                .scaleType(ImageView.ScaleType.FIT_START)
+                .build();
     }
 
 }
